3881b3e449bd0b8cdde7a7826fb2fcd00c9d296b
azharashiq178 <azharashiq178@gmail.com>
2022-07-28 02:09:52 +0500

    Initial Commit.

---
+++ AAMultiRowCollectionView/AAMultiRowCollectionView.h
@@ -0,0 +1,18 @@
+//
+//  AAMultiRowCollectionView.h
+//  AAMultiRowCollectionView
+//
+//  Created by Azher on 28/07/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AAMultiRowCollectionView.
+FOUNDATION_EXPORT double AAMultiRowCollectionViewVersionNumber;
+
+//! Project version string for AAMultiRowCollectionView.
+FOUNDATION_EXPORT const unsigned char AAMultiRowCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AAMultiRowCollectionView/PublicHeader.h>
+
+
---
+++ Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Azher on 28/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
